5fc295611f6e2fce09ca70225095d6564f04d502
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-08-21 21:10:26 +0600

    added api

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import "dotenv/config";
 import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb'
 const app = express()
 const port = process.env.PORT || 5000;
+import Stripe from 'stripe'
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 app.use(cors())
 app.use(express.json())
@@ -17,6 +19,7 @@ async function run() {
 
         const coursesCollection = client.db('pathshala').collection('courses');
         const flatsCollection = client.db('basakoi').collection('flats');
+        const reviewsCollection = client.db('basakoi').collection('reviews');
 
         app.get('/courses', async (req, res) => {
             const query = {}
@@ -50,6 +53,31 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const service = await flatsCollection.findOne(query);
             res.send(service)
+        })
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = price
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount,
+                currency: "usd",
+                payment_method_types: ['card'],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body
+            const result = await reviewsCollection.insertOne(review)
+            res.send(result)
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray()
+            res.send(result.reverse())
         })
 
     }
